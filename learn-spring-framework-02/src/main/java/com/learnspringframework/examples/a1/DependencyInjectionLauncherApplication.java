ad69cf8d2dcbbf240ef0e13f7a3c88fe314c880d
AleksanderOrlovIT <alexanderorlovkh@gmail.com>
2023-05-17 21:13:37 +0300

    Added Dependency Injection example.

--- learn-spring-framework-02/src/main/java/com/learnspringframework/examples/a1/DependencyInjectionLauncherApplication.java
+++ learn-spring-framework-02/src/main/java/com/learnspringframework/examples/a1/DependencyInjectionLauncherApplication.java
@@ -1,4 +1,4 @@
-package com.learnspringframework;
+package com.learnspringframework.examples.a1;
 
 import com.learnspringframework.game.GameRunner;
 import com.learnspringframework.game.GamingConsole;
@@ -6,13 +6,14 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.Arrays;
+
 @Configuration
-@ComponentScan("com.learnspringframework.game")
-public class GamingAppLauncherApplication {
+@ComponentScan("com.learnspringframework.examples.a1")
+public class DependencyInjectionLauncherApplication {
     public static void main(String[] args) {
-        try(var context = new AnnotationConfigApplicationContext(GamingAppLauncherApplication.class)) {
-            context.getBean(GamingConsole.class).up();
-            context.getBean(GameRunner.class).run();
+        try(var context = new AnnotationConfigApplicationContext(DependencyInjectionLauncherApplication.class)) {
+            Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
         }
     }
 }
