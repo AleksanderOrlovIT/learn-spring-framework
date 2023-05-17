c4d3c9558a5f32a831285002d707831dbec87594
AleksanderOrlovIT <alexanderorlovkh@gmail.com>
2023-05-17 11:39:00 +0300

    Added Primary and qualifier to beans.

--- src/main/java/com/in28minutes/learnspringframework/App02HelloWorldSpring.java
+++ src/main/java/com/in28minutes/learnspringframework/App02HelloWorldSpring.java
@@ -4,6 +4,9 @@ import com.in28minutes.learnspringframework.game.GameRunner;
 import com.in28minutes.learnspringframework.game.PacmanGame;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class App02HelloWorldSpring {
     public static void main(String[] args) {
         var context = new AnnotationConfigApplicationContext(HelloWorldConfiguration.class);
@@ -14,6 +17,10 @@ public class App02HelloWorldSpring {
         System.out.println(context.getBean("person2MethodCalled"));
         System.out.println(context.getBean("person3Parameters"));
         System.out.println(context.getBean("address2"));
-        //System.out.println(context.getBean(Address.class));
+        System.out.println(context.getBean(Person.class));
+        System.out.println(context.getBean(Address.class));
+        System.out.println(context.getBean("person5Qualifier"));
+
+        Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
     }
 }
