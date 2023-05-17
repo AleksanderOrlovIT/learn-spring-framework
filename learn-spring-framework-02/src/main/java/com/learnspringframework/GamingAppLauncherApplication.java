e62bac85692c43f9fcdca0a6c37a8f058998cd5d
AleksanderOrlovIT <alexanderorlovkh@gmail.com>
2023-05-17 20:49:40 +0300

    Added second module and changed to use Spring to create Beans.

---
+++ learn-spring-framework-02/src/main/java/com/learnspringframework/GamingAppLauncherApplication.java
@@ -0,0 +1,19 @@
+package com.learnspringframework;
+
+import com.learnspringframework.game.GameRunner;
+import com.learnspringframework.game.GamingConsole;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.learnspringframework.game")
+public class GamingAppLauncherApplication {
+    public static void main(String[] args) {
+        try(var context = new AnnotationConfigApplicationContext(GamingAppLauncherApplication.class)) {
+            context.getBean(GamingConsole.class).up();
+            context.getBean(GameRunner.class).run();
+        }
+
+    }
+}
