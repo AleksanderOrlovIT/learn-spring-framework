b0beb2ea8f363ee6c01a77a8fbfe9e34fb682f1c
AleksanderOrlovIT <alexanderorlovkh@gmail.com>
2023-05-17 11:23:04 +0300

    Retrieved beans using different ways.

--- src/main/java/com/in28minutes/learnspringframework/HelloWorldConfiguration.java
+++ src/main/java/com/in28minutes/learnspringframework/HelloWorldConfiguration.java
@@ -3,7 +3,7 @@ package com.in28minutes.learnspringframework;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-record Person(String name, int age){};
+record Person(String name, int age, Address address){};
 record Address(String firstLine, String city){};
 
 @Configuration
@@ -21,11 +21,26 @@ public class HelloWorldConfiguration {
 
     @Bean
     public Person person(){
-        return new Person("Ravi", 20);
+        return new Person("Ravi", 20, new Address("Main street", "Utrecht"));
     }
 
     @Bean
+    public Person person2MethodCalled(){
+        return new Person(name(), age(), address());
+    }
+
+    @Bean
+    public Person person3Parameters(String name, int age, Address address3){
+        return new Person(name, age, address3);
+    }
+
+    @Bean("address2")
     public Address address(){
         return new Address("Baker street", "London");
+    }
+
+    @Bean("address3")
+    public Address address3(){
+        return new Address("Montinagar", "Hyderabad");
     }
 }
